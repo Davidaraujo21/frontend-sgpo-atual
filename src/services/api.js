478e3dc56645ecd42d61711ce8638879e0f3de16
David <davidaraujodt51@gmail.com>
2022-01-03 03:26:22 -0300

    adiciona arquivos docker de produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:8000/api/" : "http://sgpo.eastus.cloudapp.azure.com:8000/api/",
 });
 
 
